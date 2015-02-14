300a5546bf8a701116679e3410d44c129956eef7
Jhonatan Rodrigues <jhonatan.rdgs@gmail.com>
2015-02-13 22:32:19 -0200

    Auto increment nas entidades

--- src/main/java/br/com/jhonatan/entidades/Usuario.java
+++ src/main/java/br/com/jhonatan/entidades/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="usuario")
@@ -21,6 +22,25 @@ public class Usuario implements Serializable {
 	@SequenceGenerator(name="usuario_seq", sequenceName="usuario_seq_id")
 	@GeneratedValue(generator="usuario_seq", strategy=GenerationType.SEQUENCE)
 	private Long id;
+	
+	@Transient
+	private String oi;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOi() {
+		return oi;
+	}
+
+	public void setOi(String oi) {
+		this.oi = oi;
+	}
 	
 	
 }
