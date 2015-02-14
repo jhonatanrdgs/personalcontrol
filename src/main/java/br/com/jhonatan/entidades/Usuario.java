9c466d6f71b745158ea8635241a81a5633a62405
Jhonatan Rodrigues <jhonatan.rdgs@gmail.com>
2015-02-13 22:25:51 -0200

    sequence generator

--- src/main/java/br/com/jhonatan/entidades/Usuario.java
+++ src/main/java/br/com/jhonatan/entidades/Usuario.java
@@ -2,8 +2,12 @@ package br.com.jhonatan.entidades;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +17,9 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1669857465707550409L;
 	
 	@Id
+	@Column(columnDefinition="bigserial")
+	@SequenceGenerator(name="usuario_seq", sequenceName="usuario_seq_id")
+	@GeneratedValue(generator="usuario_seq", strategy=GenerationType.SEQUENCE)
 	private Long id;
 	
 	
