844a7081a636efd7022c0877d7b22c7bc7ce0fa1
Jhonatan Rodrigues <jhonatan.rdgs@gmail.com>
2015-06-01 17:21:15 -0300

    Ideia para o adiantamento de parcelas

---
+++ src/main/java/br/com/jhonatan/controller/AdiantamentoParcelasController.java
@@ -0,0 +1,11 @@
+package br.com.jhonatan.controller;
+
+public class AdiantamentoParcelasController {
+	
+	//TODO na tela terá uma seleção de compras parcelas
+	//Assim que clicar no buscar ele tras todas as parcelas daquela compra
+	//vai ter um botão na tabela de resultados que o botão "Marcar como paga" (ou algo parecido)
+	//que irá marcar a parcela como paga (ela não deverá aparecer mais nas despesas)
+	
+
+}
