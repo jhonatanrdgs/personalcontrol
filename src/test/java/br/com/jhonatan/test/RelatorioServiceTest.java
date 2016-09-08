d59c5430cbc7207684bbee86980dcbc677f07b62
Jhonatan Rodrigues <jhonatan.rdgs@gmail.com>
2016-09-07 21:59:32 -0300

    Inicio da implementacao dos testes e aumento do timeout

---
+++ src/test/java/br/com/jhonatan/test/RelatorioServiceTest.java
@@ -0,0 +1,40 @@
+package br.com.jhonatan.test;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.jhonatan.dao.DespesaDAO;
+import br.com.jhonatan.dto.RelatorioComprasParceladasDTO;
+import br.com.jhonatan.service.RelatorioService;
+import br.com.jhonatan.service.RelatorioServiceImp;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RelatorioServiceTest {
+
+	@Mock
+	private DespesaDAO despesaDAO;
+	
+	@InjectMocks
+	private RelatorioService relatorioService = new RelatorioServiceImp();
+
+	@Before
+	public void setUp() {
+		//relatorioService = new RelatorioServiceImp();
+	}
+
+	@Test
+	public void abrirPagina() throws Exception {
+		when(despesaDAO.pesquisarDespesasParceladasMes(anyInt(), anyInt())).thenReturn(new ArrayList<RelatorioComprasParceladasDTO>());
+		assertTrue(relatorioService.pesquisarDadosRelatorioComprasParceladas(1, 1).isEmpty());
+	}
+
+}
